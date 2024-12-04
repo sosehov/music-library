b658ab42406cff1d6b42b381c1d52b0c036ee8f3
Soseh <sosehv@gmail.com>
2024-12-04 10:52:26 -0500

    Refactoring printPlaylists, printTracks and printPlaylist functions after moving all function inside the library object.

--- library.js
+++ library.js
@@ -20,51 +20,41 @@ const library = {
     name: "Other Playlist",
     tracks: ["t03"]
   }
-  }
-};
-
-/////////////////////////////
-// FUNCTIONS TO IMPLEMENT:
-/////////////////////////////
-
+  },
 // prints a list of all playlists, in the form:
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
-const printPlaylists = function(obj) {
-  const playlistsObject = obj.playlists;
-  for (const p in playlistsObject) {
-    let playlistName = playlistsObject[p].name;
-    let arrayOfTracks = playlistsObject[p].tracks;
-    let tracksCount = arrayOfTracks.length;
-    console.log(`${p}: ${playlistName} - ${tracksCount} tracks`); //need to refactor and print "track" if the number of tracks is 1
-  }
-};
-//TEST CODE
-printPlaylists(library);
-
+  printPlaylists: function() {
+    const playlistsObject = this.playlists;
+    for (const p in playlistsObject) {
+      let playlistName = playlistsObject[p].name;
+      let arrayOfTracks = playlistsObject[p].tracks;
+      let tracksCount = arrayOfTracks.length;
+      //check if tracksCount is 1 or more and print accordingly
+      let trackLabel = tracksCount === 1 ? "track" : "tracks";
+      console.log(`${p}: ${playlistName} - ${tracksCount} ${trackLabel}`); //
+    }
+  },
 // prints a list of all tracks, using the following format:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
-const printTracks = function(obj) {
-  const tracksObject = obj.tracks;
-  for (const t in tracksObject) {
-    let trackName = tracksObject[t].name; //why can't I write tracksObject.t.name?
-    let trackArtist = tracksObject[t].artist;
-    let trackAlbum = tracksObject[t].album;
-    console.log(`${t}: ${trackName} by ${trackArtist} (${trackAlbum})`);
-  }
-};
-//TEST CODE
-printTracks(library);
-
+  printTracks: function() {
+       const tracksObject = this.tracks;
+       for (const t in tracksObject) {
+       let trackName = tracksObject[t].name; //why can't I write tracksObject.t.name?
+       let trackArtist = tracksObject[t].artist;
+       let trackAlbum = tracksObject[t].album;
+       console.log(`${t}: ${trackName} by ${trackArtist} (${trackAlbum})`);
+       }
+},
 // prints a list of tracks for a given playlist, using the following format:
 // p01: Coding Music - 2 tracks
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
-const playlistsObject = library["playlists"];
-const tracksObject = library["tracks"];
-const printPlaylist = function(playlistId) {
+  printPlaylist: function(playlistId) {
+  const playlistsObject = library["playlists"];
+  const tracksObject = library["tracks"];
   const trackName = playlistsObject[playlistId]["name"];
   const playlistTracks = playlistsObject[playlistId]["tracks"];
   const tracksCount = playlistTracks.length;
@@ -75,75 +65,75 @@ const printPlaylist = function(playlistId) {
     let trackAlbum = tracksObject[t]["album"];
     console.log(`${t}: ${trackName} by ${trackArtist} (${trackAlbum})`);
   }
-};
-//TEST CODE
-printPlaylist("p01");
-printPlaylist("p02");
-
-// adds an existing track to an existing playlist
-const addTrackToPlaylist = function(trackId, playlistId) {
-  const playlistsObject = library["playlists"];
-  let tracksArray = playlistsObject[playlistId]["tracks"];
-  if (!tracksArray.includes(trackId)) {
-    tracksArray.push(trackId);
-  }
+  },
+  // adds an existing track to an existing playlist
+  addTrackToPlaylist: function(trackId, playlistId) {
+    const playlistsObject = library["playlists"];
+    let tracksArray = playlistsObject[playlistId]["tracks"];
+    if (!tracksArray.includes(trackId)) {
+       tracksArray.push(trackId);
+    }
   return tracksArray.sort();
+  },
+// generates a unique id
+// (already implemented: use this for addTrack and addPlaylist)
+  generateUid: function() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  },
+// adds a track to the library
+  addTrack: function(name, artist, album) {
+    let tracksObject = library.tracks;
+    //generate a unique track ID
+    let trackId = "t" + generateUid();
+    const allTrackIds = Object.keys(tracksObject);
+    //check for uniqueness
+    if (allTrackIds.includes(trackId)) {
+    console.log("This track ID already exists, regenerating...");
+    let trackId = "t" + generateUid(); //handle the case when the newly generated ID also exists
+    } else {
+       tracksObject[trackId] = {
+       id:  trackId,
+       name: name,
+       artist:  artist,
+       album:  album,
+       };
+       console.log(`Track ${trackId} added.`);
+       }
+},
+// adds a playlist to the library
+  addPlaylist: function(name) {
+    const playlistsObject = library["playlists"];
+    let playlistId = "t" + generateUid();
+    const allPlaylistsId = Object.keys(playlistsObject);
+    if (allPlaylistsId.includes(playlistId)) {
+      console.log("This playlist already exists, regenerating...");
+      let playlistId = "t" + generateUid(); //handle the case when the newly generated ID also exists
+    } else {
+       playlistsObject[playlistId] = {
+       id:  playlistId,
+       name: name,
+       tracks:  "t01"
+       };
+       console.log(`Playlist ${playlistId} added.`);
+       }
+  },
 };
+
+
+//TEST CODE
+library.printPlaylists();
+library.printTracks();
+library.printPlaylist("p01");
+library.printPlaylist("p02");
+
 //TEST CODE
 console.log(addTrackToPlaylist("t01", "p02"));
 console.log(addTrackToPlaylist("t03", "p01"));
 console.log(addTrackToPlaylist("t03", "p02"));//testing for duplicates
 
-// generates a unique id
-// (already implemented: use this for addTrack and addPlaylist)
-const generateUid = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-};
-
-// adds a track to the library
-const addTrack = function(name, artist, album) {
-  let tracksObject = library.tracks;
-  //generate a unique track ID
-  let trackId = "t" + generateUid();
-  const allTrackIds = Object.keys(tracksObject);
-  //check for uniqueness
-  if (allTrackIds.includes(trackId)) {
-    console.log("This track ID already exists, regenerating...");
-    let trackId = "t" + generateUid(); //handle the case when the newly generated ID also exists
-  } else {
-    tracksObject[trackId] = {
-      id:  trackId,
-      name: name,
-      artist:  artist,
-      album:  album,
-    };
-    console.log(`Track ${trackId} added.`);
-  }
-};
 //TEST CODE
 addTrack("Believe","Cher","Runaway");
-
-// adds a playlist to the library
-const addPlaylist = function(name) {
-  const playlistsObject = library["playlists"];
-  let playlistId = "t" + generateUid();
-  const allPlaylistsId = Object.keys(playlistsObject);
-  if (allPlaylistsId.includes(playlistId)) {
-       console.log("This playlist already exists, regenerating...");
-       let playlistId = "t" + generateUid(); //handle the case when the newly generated ID also exists
-     } else {
-       playlistsObject[playlistId] = {
-         id:  playlistId,
-         name: name,
-         tracks:  "t01"
-       };
-       console.log(`Playlist ${playlistId} added.`);
-     }
-   };
-//TEST CODE
-addPlaylist("Chilling Music");
-
-// STRETCH:
+ STRETCH:
 // given a query string string, prints a list of tracks
 // where the name, artist or album contains the query string (case insensitive)
 // tip: use "string".search("tri") 
